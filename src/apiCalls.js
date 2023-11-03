91ee4771f47d0272a673fdd46c3aa94dd9bffaa5
javier-aguilar <jvr.agx@gmail.com>
2023-11-02 18:41:48 -0600

    Add getData function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,18 @@
-// Your fetch requests will live here!
+const getData = async (type) => {
+    try {
+        const response = await fetch(`http://localhost:3001/api/v1/${type}`).then(response => {
+            if (!response.ok) {
+                throw new Error(`${response.status} ${response.statusText} `);
+            }
+            return response.json();
+            
+        });
+        return response;       
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-
-console.log('I will be a fetch request!')
-
+module.exports = {
+    getData
+} 
